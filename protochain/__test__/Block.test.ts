7f944745f71ca77e7569220395d5082958d2d78b
Fábio <fabio.oliveira@poscontrole.com.br>
2023-04-27 21:06:53 -0300

    atualizar classe Block

--- protochain/__test__/Block.test.ts
+++ protochain/__test__/Block.test.ts
@@ -1,22 +1,49 @@
-import { describe, expect, test } from "vitest";
+import { beforeAll, describe, expect, test } from "vitest";
 import Block from "../src/lib/Block";
 
 describe("Block tests", () => {
+  let genesis: Block;
+
+  beforeAll(() => {
+    genesis = new Block(0, "", "Genesis Block");
+  });
+
   test("Shuld be valid", () => {
-    const block = new Block(1, "abc");
-    const valid = block.isValid();
-    expect(valid).toBeTruthy();
+    const block = new Block(1, genesis.hash, "block 2");
+    const valid = block.isValid(genesis.hash, genesis.index);
+    expect(valid.success).toBeTruthy();
+  });
+
+  test("Shuld Not be valid (previous hash)", () => {
+    const block = new Block(1, "abc", "block 2");
+    const valid = block.isValid(genesis.hash, genesis.index);
+    expect(valid.success).toBeFalsy();
+  });
+
+  test("Shuld Not be valid (timestamp)", () => {
+    const block = new Block(1, genesis.hash, "block 2");
+    block.timestamp = -1;
+    block.hash = block.getHash();
+    const valid = block.isValid(genesis.hash, genesis.index);
+    expect(valid.success).toBeFalsy();
   });
 
   test("Shuld Not be valid (hash)", () => {
-    const block = new Block(1, "");
-    const valid = block.isValid();
-    expect(valid).toBeFalsy();
+    const block = new Block(1, genesis.hash, "block 2");
+    block.hash = "";
+    const valid = block.isValid(genesis.hash, genesis.index);
+    expect(valid.success).toBeFalsy();
+  });
+
+  test("Shuld Not be valid (data)", () => {
+    const block = new Block(1, genesis.hash, "");
+    const valid = block.isValid(genesis.hash, genesis.index);
+    expect(valid.success).toBeFalsy();
   });
 
   test("Shuld Not be valid (index)", () => {
-    const block = new Block(-1, "abc");
-    const valid = block.isValid();
-    expect(valid).toBeFalsy();
+    const block = new Block(-1, genesis.hash, "block 2");
+    const valid = block.isValid(genesis.hash, genesis.index);
+    expect(valid.success).toBeFalsy();
   });
 });
--- protochain/src/lib/Block.ts
+++ protochain/src/lib/Block.ts
@@ -1,29 +1,49 @@
+import * as crypto from "crypto";
+import Validation from "./Validation";
 /**
  * Block class
  */
 export default class Block {
   index: number;
+  timestamp: number;
   hash: string;
+  previousHash: string;
+  data: string;
 
   /**
    * Creates a new block
    * @param index The block index in blockchain
-   * @param hash The block hash
+   * @param previousHash The previous block hash
+   * @param data The block data
    */
-  constructor(index: number, hash: string) {
-    this.hash = hash;
+  constructor(index: number, previousHash: string, data: string) {
     this.index = index;
+    this.timestamp = Date.now();
+    this.previousHash = previousHash;
+    this.data = data;
+    this.hash = this.getHash();
+  }
+
+  getHash(): string {
+    const data = this.index + this.data + this.timestamp + this.previousHash;
+    const hash = crypto.createHash("sha256").update(data).digest("hex");
+    return hash;
   }
 
   /**
    * Validates the
    * @returns Returns true if the block is valid
    */
-  isValid() {
-    if (this.hash.trim() === "") return false;
+  isValid(previousHash: string, previousIndex: number) {
+    if (previousIndex !== this.index - 1)
+      return new Validation(false, "Invalid index.");
+    if (this.hash !== this.getHash())
+      return new Validation(false, "Invalid hash.");
+    if (!this.data) return new Validation(false, "Invalid data.");
+    if (this.timestamp < 1) return new Validation(false, "Invalid timestamp.");
+    if (this.previousHash !== previousHash)
+      return new Validation(false, "Invalid previous hash.");
 
-    if (this.index < 1) return false;
-
-    return true;
+    return new Validation();
   }
 }
