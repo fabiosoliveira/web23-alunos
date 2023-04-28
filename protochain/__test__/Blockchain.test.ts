9eb30f950e49c7da4400075d16f35aefe17c76e0
Fábio <fabio.oliveira@poscontrole.com.br>
2023-04-27 21:06:37 -0300

    atualizar classe Blockchain

--- protochain/__test__/Blockchain.test.ts
+++ protochain/__test__/Blockchain.test.ts
@@ -1,9 +1,44 @@
 import { describe, expect, test } from "vitest";
 import Blockchain from "../src/lib/Blockchain";
+import Block from "../src/lib/Block";
 
 describe("Blockchain tests", () => {
   test("Shuld has genesis blocks", () => {
     const blockchain = new Blockchain();
     expect(blockchain.blocks.length).toEqual(1);
+  });
+
+  test("Shuld be valid (genesis)", () => {
+    const blockchain = new Blockchain();
+    expect(blockchain.isValid().success).toEqual(true);
+  });
+
+  test("Shuld be valid (two blocks)", () => {
+    const blockchain = new Blockchain();
+    blockchain.addBlock(new Block(1, blockchain.blocks[0].hash, "Block 2"));
+    expect(blockchain.isValid().success).toEqual(true);
+  });
+
+  test("Shuld not be valid (two blocks)", () => {
+    const blockchain = new Blockchain();
+    blockchain.addBlock(new Block(1, blockchain.blocks[0].hash, "Block 2"));
+    blockchain.blocks[1].data = "a transfere 2 para b";
+    expect(blockchain.isValid().success).toEqual(false);
+  });
+
+  test("Shuld add block", () => {
+    const blockchain = new Blockchain();
+    const result = blockchain.addBlock(
+      new Block(1, blockchain.blocks[0].hash, "Block 2")
+    );
+    expect(result.success).toEqual(true);
+  });
+
+  test("Shuld not add block", () => {
+    const blockchain = new Blockchain();
+    const result = blockchain.addBlock(
+      new Block(-1, blockchain.blocks[0].hash, "Block 2")
+    );
+    expect(result.success).toEqual(false);
   });
 });
--- protochain/src/lib/Blockchain.ts
+++ protochain/src/lib/Blockchain.ts
@@ -1,15 +1,52 @@
 import Block from "./Block";
+import Validation from "./Validation";
 
 /**
  * Blockchain class
  */
 export default class Blockchain {
   blocks: Block[];
+  nextIndex = 0;
 
   /**
    * Creates a new blockchain
    */
   constructor() {
-    this.blocks = [new Block(0, "genesis")];
+    this.blocks = [new Block(this.nextIndex, "", "Genesis Block")];
+    this.nextIndex++;
+  }
+
+  getLastBlock(): Block {
+    return this.blocks[this.blocks.length - 1];
+  }
+
+  addBlock(block: Block): Validation {
+    const lastBlock = this.getLastBlock();
+
+    const validation = block.isValid(lastBlock.hash, lastBlock.index);
+    if (!validation.success)
+      return new Validation(false, `Invalid block: ${validation.message}`);
+
+    this.blocks.push(block);
+    this.nextIndex++;
+
+    return new Validation();
+  }
+
+  isValid(): Validation {
+    for (let i = this.blocks.length - 1; i > 0; i--) {
+      const currentBlock = this.blocks[i];
+      const previousBlock = this.blocks[i - 1];
+      const validation = currentBlock.isValid(
+        previousBlock.hash,
+        previousBlock.index
+      );
+      if (!validation.success)
+        return new Validation(
+          false,
+          `Invalid block #${currentBlock.index}: ${validation.message}`
+        );
+    }
+    return new Validation();
   }
 }
