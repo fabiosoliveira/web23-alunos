13a1c4fd1763d2ec6b81a6194e1032769f9eaf9a
Fábio <fabio.oliveira@poscontrole.com.br>
2023-06-14 17:13:55 -0300

    create contract ProtoCoin

---
+++ protocoin-bep20/contracts/ProtoCoin.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ProtoCoin is ERC20 {
+
+    constructor() ERC20("ProtoCoin", "PRC") {
+        _mint(msg.sender, 1000 * 10 ** 18);
+    }
+}
