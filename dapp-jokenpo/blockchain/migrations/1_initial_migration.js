e28de0437905c8faae65088574245e4d20ea1801
Fábio <fabio.oliveira@poscontrole.com.br>
2023-06-29 15:41:11 -0300

    Deploy do Contrato

--- dapp-jokenpo/blockchain/migrations/1_initial_migration.js
+++ dapp-jokenpo/blockchain/migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
+const JKPLibrary = artifacts.require("JKPLibrary");
 const JoKenPo = artifacts.require("JoKenPo");
+const JKPAdapter = artifacts.require("JKPAdapter");
 
 module.exports = function (deployer) {
+  deployer.deploy(JKPLibrary);
   deployer.deploy(JoKenPo);
+  deployer.deploy(JKPAdapter);
 };
