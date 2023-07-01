8fe4b7b382a4fe95008c63e8e5c556024f27b3b0
Fábio <fabio.oliveira@poscontrole.com.br>
2023-07-01 14:51:34 -0300

    Área de Leaderboard

--- dapp-jokenpo/frontend/src/App.tsx
+++ dapp-jokenpo/frontend/src/App.tsx
@@ -49,49 +49,77 @@ function App() {
           <p className="lead text-danger">{message}</p>
         </div>
         <div className="col-md-8 col-lg-12">
-          <div className="col-sm-6"></div>
-          <div className="col-sm-6">
-            <h4 className="mb-3">Games</h4>
-            <div className="card card-body border-0 shadow">
-              <h5 className="mb-3 text-primary">Current Status:</h5>
-              <div className="alert alert-success">
-                {leaderboard && leaderboard.result
-                  ? leaderboard.result
-                  : "Loading..."}
+          <div className="row">
+            <div className="col-sm-6">
+              <h4 className="mb-3">Best Players</h4>
+              <div className="card card-body border-0 shadow table-wrapper table-responsive ">
+                <table className="table table-hover">
+                  <thead>
+                    <tr>
+                      <th className="border-gray-200">Player</th>
+                      <th className="border-gray-200">Wins</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {leaderboard?.players?.length ? (
+                      leaderboard.players.map((player) => (
+                        <tr key={player.wallet}>
+                          <td className="border-gray-200">{player.wallet}</td>
+                          <td className="border-gray-200">{player.wins}</td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td colSpan={2}>Loading...</td>
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
               </div>
-              <h5 className="mb-3 text-primary">
-                {(leaderboard && leaderboard.result?.indexOf("won") !== -1) ||
-                !leaderboard?.result
-                  ? "Start a new game:"
-                  : "Play this game:"}
-              </h5>
-              <div className="d-flex">
-                <div className="col-sm-4">
-                  <div
-                    onClick={() => onPlay(Options.PAPER)}
-                    className="alert alert-info play-button me-3"
-                  >
-                    <img src="src/assets/paper.png" width={100} alt="Paper" />
-                  </div>
+            </div>
+            <div className="col-sm-6">
+              <h4 className="mb-3">Games</h4>
+              <div className="card card-body border-0 shadow">
+                <h5 className="mb-3 text-primary">Current Status:</h5>
+                <div className="alert alert-success">
+                  {leaderboard && leaderboard.result
+                    ? leaderboard.result
+                    : "Loading..."}
                 </div>
-                <div className="col-sm-4">
-                  <div
-                    onClick={() => onPlay(Options.ROCK)}
-                    className="alert alert-info play-button"
-                  >
-                    <img src="src/assets/rock.png" width={100} alt="Rock" />
+                <h5 className="mb-3 text-primary">
+                  {(leaderboard && leaderboard.result?.indexOf("won") !== -1) ||
+                  !leaderboard?.result
+                    ? "Start a new game:"
+                    : "Play this game:"}
+                </h5>
+                <div className="d-flex">
+                  <div className="col-sm-4">
+                    <div
+                      onClick={() => onPlay(Options.PAPER)}
+                      className="alert alert-info play-button me-3"
+                    >
+                      <img src="src/assets/paper.png" width={100} alt="Paper" />
+                    </div>
                   </div>
-                </div>
-                <div className="col-sm-4">
-                  <div
-                    onClick={() => onPlay(Options.SCISSORS)}
-                    className="alert alert-info play-button ms-3"
-                  >
-                    <img
-                      src="src/assets/scissors.png"
-                      width={100}
-                      alt="Scissors"
-                    />
+                  <div className="col-sm-4">
+                    <div
+                      onClick={() => onPlay(Options.ROCK)}
+                      className="alert alert-info play-button"
+                    >
+                      <img src="src/assets/rock.png" width={100} alt="Rock" />
+                    </div>
+                  </div>
+                  <div className="col-sm-4">
+                    <div
+                      onClick={() => onPlay(Options.SCISSORS)}
+                      className="alert alert-info play-button ms-3"
+                    >
+                      <img
+                        src="src/assets/scissors.png"
+                        width={100}
+                        alt="Scissors"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
