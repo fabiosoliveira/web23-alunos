4454baa0d43e54c10eba0d2d1f54ad34e206dc1a
Fábio <fabio.oliveira@poscontrole.com.br>
2023-07-03 16:07:48 -0300

    Modifiers, Primeiras Funções e Testes

---
+++ crud-hardhat/test/Condominium.test.ts
@@ -0,0 +1,26 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Condominium", function () {
+  async function deployFixture() {
+    const [manager, resident] = await ethers.getSigners();
+
+    const Condominium = await ethers.getContractFactory("Condominium");
+    const contract = await Condominium.deploy();
+
+    return { contract, manager, resident };
+  }
+
+  it("Should be residence", async function () {
+    const { contract, manager, resident } = await loadFixture(deployFixture);
+
+    expect(await contract.residenceExists(2102)).to.equal(true);
+  });
+
+  it("Should be resident", async function () {
+    const { contract, manager, resident } = await loadFixture(deployFixture);
+
+    expect(await contract.isResident(resident.address)).to.equal(true);
+  });
+});
