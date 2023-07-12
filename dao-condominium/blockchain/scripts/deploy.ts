b0aa55eac22972e939cece215858b3f237478ff0
Fábio <fabio.oliveira@poscontrole.com.br>
2023-07-12 15:07:20 -0300

    Deploy e Verificação

--- dao-condominium/blockchain/scripts/deploy.ts
+++ dao-condominium/blockchain/scripts/deploy.ts
@@ -1,11 +1,32 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  // deploy the CondominiumLib
+  const CondominiumLib = await ethers.getContractFactory("CondominiumLib");
+  const contractLib = await CondominiumLib.deploy();
+
+  await contractLib.deployed();
+  console.log(`CondominiumLib deployed to: ${contractLib.address}`);
+
+  // deploy the Condominium
   const Condominium = await ethers.getContractFactory("Condominium");
   const contract = await Condominium.deploy();
 
   await contract.deployed();
   console.log(`Contract deployed to: ${contract.address}`);
+
+  // deploy the CondominiumAdapter
+  const CondominiumAdapter = await ethers.getContractFactory(
+    "CondominiumAdapter"
+  );
+  const adapter = await CondominiumAdapter.deploy();
+
+  await adapter.deployed();
+  console.log(`CondominiumAdapter deployed to: ${adapter.address}`);
+
+  // upgrade contract
+  await adapter.upgrade(contract.address);
+  console.log(`CondominiumAdapter upgraded to: ${contract.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
