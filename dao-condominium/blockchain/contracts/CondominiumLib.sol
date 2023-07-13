eb2af8525025457aaa738d9345ed7014862c6976
Fábio <fabio.oliveira@poscontrole.com.br>
2023-07-13 15:19:55 -0300

    Refactor ICondominium e Adapter

--- dao-condominium/blockchain/contracts/CondominiumLib.sol
+++ dao-condominium/blockchain/contracts/CondominiumLib.sol
@@ -57,4 +57,21 @@ library CondominiumLib {
         uint amount;
         string topic;
     }
+
+    struct Resident {
+        address wallet;
+        uint16 residence;
+        bool isCounselor;
+        bool isManager;
+    }
+
+    struct ResidentPage {
+        Resident[] residents;
+        uint total;
+    }
+
+    struct TopicPage {
+        Topic[] topics;
+        uint total;
+    }
 } 
