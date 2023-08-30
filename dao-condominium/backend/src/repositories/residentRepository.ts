5b9b30a1db7ff875b775ee074967c37eee453682
Fábio <fabio.oliveira@poscontrole.com.br>
2023-08-30 16:21:06 -0300

    residentRepository e resident.ts

---
+++ dao-condominium/backend/src/repositories/residentRepository.ts
@@ -0,0 +1,50 @@
+import Resident from "src/models/resident";
+import connet from "../db";
+
+const COLLECTION = "residents";
+
+async function getResident(wallet: string): Promise<Resident | null> {
+  const db = await connet();
+  const resident = await db.collection(COLLECTION).findOne({ wallet });
+  if (!resident) return null;
+
+  return new Resident(
+    resident.wallet,
+    resident.name,
+    resident.profile,
+    resident.phone,
+    resident.email
+  );
+}
+
+async function addResident(resident: Resident): Promise<Resident> {
+  const db = await connet();
+  const result = await db.collection(COLLECTION).insertOne(resident);
+  resident._id = result.insertedId;
+
+  return resident;
+}
+
+async function updateResident(
+  wallet: string,
+  data: Resident
+): Promise<Resident | null> {
+  const db = await connet();
+  await db.collection(COLLECTION).updateOne({ wallet }, { $set: data });
+
+  return getResident(wallet);
+}
+
+async function deleteResident(wallet: string): Promise<boolean> {
+  const db = await connet();
+  const result = await db.collection(COLLECTION).deleteOne({ wallet });
+
+  return result.deletedCount > 0;
+}
+
+export default {
+  getResident,
+  addResident,
+  updateResident,
+  deleteResident,
+};
