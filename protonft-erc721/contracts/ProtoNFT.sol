44d9e56b6229741a8d0e6857834bde07f48c534e
Fábio <fabio.oliveira@poscontrole.com.br>
2023-09-26 10:11:41 -0300

    OpenZeppelin e ERC-721

--- protonft-erc721/contracts/ProtoNFT.sol
+++ protonft-erc721/contracts/ProtoNFT.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract ProtoNFT {
+contract ProtoNFT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable {
     
 }
