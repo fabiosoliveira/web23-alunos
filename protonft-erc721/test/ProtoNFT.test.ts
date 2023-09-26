0325d17dfaa40c0a5dfe662dcd73ae5474846859
Fábio <fabio.oliveira@poscontrole.com.br>
2023-09-26 15:11:00 -0300

    Finalizando os Testes de NFT

--- protonft-erc721/test/ProtoNFT.test.ts
+++ protonft-erc721/test/ProtoNFT.test.ts
@@ -67,6 +67,7 @@ describe("ProtoNFT", function () {
     const tokenId = await contract.tokenByIndex(0);
 
     await contract.approve(otherAccount.address, tokenId);
+    const approved = await contract.getApproved(tokenId);
 
     const instance = contract.connect(otherAccount);
     await instance.burn(tokenId);
@@ -76,6 +77,7 @@ describe("ProtoNFT", function () {
 
     expect(balance).to.equal(0, "Can't burn (approved)");
     expect(totalSupply).to.equal(0, "Can't burn (approved)");
+    expect(approved).to.equal(otherAccount.address, "Can't burn (approved)");
   });
 
   it("Should burn (approved for all)", async function () {
@@ -85,6 +87,10 @@ describe("ProtoNFT", function () {
     const tokenId = await contract.tokenByIndex(0);
 
     await contract.setApprovalForAll(otherAccount.address, true);
+    const approved = await contract.isApprovedForAll(
+      owner.address,
+      otherAccount.address
+    );
 
     const instance = contract.connect(otherAccount);
     await instance.burn(tokenId);
@@ -94,6 +100,7 @@ describe("ProtoNFT", function () {
 
     expect(balance).to.equal(0, "Can't burn (approved for all)");
     expect(totalSupply).to.equal(0, "Can't burn (approved for all)");
+    expect(approved).to.equal(true, "Can't burn (approved for all)");
   });
 
   it("Should NOT burn (not exists)", async function () {
@@ -133,6 +140,153 @@ describe("ProtoNFT", function () {
 
     await expect(contract.tokenURI(1)).to.be.revertedWith(
       "ERC721: invalid token ID"
+    );
+  });
+
+  it("Should transfer", async function () {
+    const { contract, owner, otherAccount } = await loadFixture(deployFixture);
+
+    await contract.mint();
+    const tokenId = await contract.tokenByIndex(0);
+
+    await contract.transferFrom(owner.address, otherAccount.address, tokenId);
+    const balanceFrom = await contract.balanceOf(owner.address);
+    const balanceTo = await contract.balanceOf(otherAccount.address);
+    const ownerOf = await contract.ownerOf(tokenId);
+    const ownerTokenId = await contract.tokenOfOwnerByIndex(
+      otherAccount.address,
+      0
+    );
+
+    expect(balanceFrom).to.equal(0, "Can't transfer");
+    expect(balanceTo).to.equal(1, "Can't transfer");
+    expect(ownerOf).to.equal(otherAccount.address, "Can't transfer");
+    expect(tokenId).to.equal(ownerTokenId, "Can't transfer");
+  });
+
+  it("Should emit transfer", async function () {
+    const { contract, owner, otherAccount } = await loadFixture(deployFixture);
+
+    await contract.mint();
+    const tokenId = await contract.tokenByIndex(0);
+
+    await expect(
+      contract.transferFrom(owner.address, otherAccount.address, tokenId)
+    )
+      .to.emit(contract, "Transfer")
+      .withArgs(owner.address, otherAccount.address, tokenId);
+  });
+
+  it("Should transfer (approved)", async function () {
+    const { contract, owner, otherAccount } = await loadFixture(deployFixture);
+
+    await contract.mint();
+    const tokenId = await contract.tokenByIndex(0);
+
+    await contract.approve(otherAccount.address, tokenId);
+    const approved = await contract.getApproved(tokenId);
+
+    const instance = contract.connect(otherAccount);
+    await instance.transferFrom(owner.address, otherAccount.address, tokenId);
+
+    const ownerOf = await contract.ownerOf(tokenId);
+
+    expect(ownerOf).to.equal(otherAccount.address, "Can't transfer (approved)");
+    expect(approved).to.equal(
+      otherAccount.address,
+      "Can't transfer (approved)"
+    );
+  });
+
+  it("Should emit approval", async function () {
+    const { contract, owner, otherAccount } = await loadFixture(deployFixture);
+
+    await contract.mint();
+    const tokenId = await contract.tokenByIndex(0);
+
+    await expect(contract.approve(otherAccount.address, tokenId))
+      .to.emit(contract, "Approval")
+      .withArgs(owner.address, otherAccount.address, tokenId);
+  });
+
+  it("Should clear approvals", async function () {
+    const { contract, owner, otherAccount } = await loadFixture(deployFixture);
+
+    await contract.mint();
+    const tokenId = await contract.tokenByIndex(0);
+
+    await contract.approve(otherAccount.address, tokenId);
+
+    await contract.transferFrom(owner.address, otherAccount.address, tokenId);
+
+    const approved = await contract.getApproved(tokenId);
+
+    expect(approved).to.equal(
+      "0x0000000000000000000000000000000000000000",
+      "Can't clear approvals"
+    );
+  });
+
+  it("Should transfer (approved for all)", async function () {
+    const { contract, owner, otherAccount } = await loadFixture(deployFixture);
+
+    await contract.mint();
+    const tokenId = await contract.tokenByIndex(0);
+
+    await contract.setApprovalForAll(otherAccount.address, true);
+    const approved = await contract.isApprovedForAll(
+      owner.address,
+      otherAccount.address
+    );
+
+    const instance = contract.connect(otherAccount);
+    await instance.transferFrom(owner.address, otherAccount.address, tokenId);
+
+    const ownerOf = await contract.ownerOf(tokenId);
+
+    expect(ownerOf).to.equal(
+      otherAccount.address,
+      "Can't transfer (approved for all)"
+    );
+    expect(approved).to.equal(true, "Can't transfer (approved for all)");
+  });
+
+  it("Should emit approval for all", async function () {
+    const { contract, owner, otherAccount } = await loadFixture(deployFixture);
+
+    await contract.mint();
+
+    await expect(contract.setApprovalForAll(otherAccount.address, true))
+      .to.emit(contract, "ApprovalForAll")
+      .withArgs(owner.address, otherAccount.address, true);
+  });
+
+  it("Should NOT transfer (permission)", async function () {
+    const { contract, owner, otherAccount } = await loadFixture(deployFixture);
+
+    await contract.mint();
+    const tokenId = await contract.tokenByIndex(0);
+
+    const instance = contract.connect(otherAccount);
+    await expect(
+      instance.transferFrom(owner.address, otherAccount.address, tokenId)
+    ).to.be.rejectedWith("ERC721: caller is not token owner or approved");
+  });
+
+  it("Should NOT transfer (exists)", async function () {
+    const { contract, owner, otherAccount } = await loadFixture(deployFixture);
+
+    await expect(
+      contract.transferFrom(owner.address, otherAccount.address, 1)
+    ).to.be.rejectedWith("ERC721: invalid token ID");
+  });
+
+  it("Should supports interface", async function () {
+    const { contract } = await loadFixture(deployFixture);
+
+    expect(await contract.supportsInterface("0x80ac58cd")).to.equal(
+      true,
+      "Can't supports interface"
     );
   });
 });
