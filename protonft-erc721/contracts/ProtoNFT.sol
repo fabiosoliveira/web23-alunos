5915189996784a73439fa030fead1d144a9d4ea7
Fábio <fabio.oliveira@poscontrole.com.br>
2023-09-26 10:37:08 -0300

    Implementando o contrato ProtoNFT

--- protonft-erc721/contracts/ProtoNFT.sol
+++ protonft-erc721/contracts/ProtoNFT.sol
@@ -12,5 +12,38 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 // import "hardhat/console.sol";
 
 contract ProtoNFT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable {
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
     constructor() ERC721("ProtoNFT", "PNFT") {}
+
+    function  mint() public {
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, Strings.toString(tokenId));
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://www.fabio.com.br/nfts/";
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return string.concat(super.tokenURI(tokenId), ".json");
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
 }
