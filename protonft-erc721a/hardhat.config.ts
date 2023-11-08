7132832fc5b36a3598c587b3e90bfb6d5a443a86
Fábio <fabio.oliveira@poscontrole.com.br>
2023-11-08 15:50:35 -0300

    Deploy do ProtoNFT na Avax

--- protonft-erc721a/hardhat.config.ts
+++ protonft-erc721a/hardhat.config.ts
@@ -1,8 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.17",
+  networks: {
+    avaxtest: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      accounts: {
+        mnemonic: process.env.SECRET,
+      },
+    },
+    bsctest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts: {
+        mnemonic: process.env.SECRET,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
 };
 
 export default config;
