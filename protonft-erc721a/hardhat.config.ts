d0f50fbceacae805917c85e89e6763c529397ac2
Fábio <fabio.oliveira@poscontrole.com.br>
2023-11-30 16:21:42 -0300

    fix Error HH210

--- protonft-erc721a/hardhat.config.ts
+++ protonft-erc721a/hardhat.config.ts
@@ -1,6 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-etherscan";
+// import "@nomiclabs/hardhat-etherscan";
 
 import dotenv from "dotenv";
 dotenv.config();
