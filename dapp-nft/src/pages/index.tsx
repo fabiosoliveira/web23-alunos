7bc20dcdfe12ef810c417808d11c8b0fab62634c
Fábio <fabio.oliveira@poscontrole.com.br>
2023-12-01 15:25:25 -0300

    Customizando o Centro

--- dapp-nft/src/pages/index.tsx
+++ dapp-nft/src/pages/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import Layout from "../components/Layout";
 import Image from "next/image";
 
@@ -6,7 +7,6 @@ const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <Hero />
     <Services />
-    <Featured />
     <Team />
     <Finisher />
   </Layout>
@@ -123,34 +123,28 @@ const Services = () => (
 
       <div className="flex flex-wrap items-center mt-32">
         <div className="w-full md:w-5/12 px-4 mr-auto ml-auto">
-          <div className="text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-gray-100">
-            <i className="fas fa-user-friends text-xl"></i>
-          </div>
           <h3 className="text-3xl mb-2 font-semibold leading-normal">
-            Working with us is a pleasure
+            And more is coming...
           </h3>
           <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
-            Don't let your uses guess by attaching tooltips and popoves to any
-            element. Just make sure you enable them first via JavaScript.
+            This NFT collection is not just about cool images tailored for you.
+            I'm working to give more exclusive experiences and content to our
+            community.
           </p>
           <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-700">
-            The kit comes with three pre-built pages to help you get started
-            faster. You can change the text and images and you're good to go.
-            Just make sure you enable them first via JavaScript.
+            If you join us now, you will be investing in your future as a web3
+            dev and in a huge ecossystem that is only debuting.
           </p>
-          <a
-            href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/presentation"
-            className="font-bold text-gray-800 mt-8"
-          >
-            Check Tailwind Starter Kit!
-          </a>
+          <Link href="/mint" className="font-bold text-gray-800 mt-8">
+            Mint now your token!
+          </Link>
         </div>
 
         <div className="w-full md:w-4/12 px-4 mr-auto ml-auto">
           <div className="relative flex flex-col min-w-0 break-words  w-full mb-6 shadow-lg rounded-lg bg-pink-600">
             <img
               alt="..."
-              src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
+              src="https://livecoins.com.br/wp-content/uploads/2022/02/BNB-CHain.jpg"
               className="w-full align-middle rounded-t-lg"
             />
             <blockquote className="relative p-8 mb-4">
@@ -170,103 +164,14 @@ const Services = () => (
                 ></polygon>
               </svg>
               <h4 className="text-xl font-bold text-white">
-                Top Notch Services
+                Top Notch Blockchain
               </h4>
               <p className="text-md font-light mt-2 text-white">
-                The Arctic Ocean freezes every winter and much of the sea-ice
-                then thaws every summer, and that process will continue whatever
-                happens.
+                Our collection is built on top of BNB Smart Chain (formeerly
+                know as BSC), mantained by the #1 crypto exchange in the world:
+                Binance.
               </p>
             </blockquote>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-);
-
-const Featured = () => (
-  <section className="relative py-20">
-    <div
-      className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
-      style={{ height: "80px" }}
-    >
-      <svg
-        className="absolute bottom-0 overflow-hidden"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="none"
-        version="1.1"
-        viewBox="0 0 2560 100"
-        x="0"
-        y="0"
-      >
-        <polygon
-          className="text-white fill-current"
-          points="2560 0 2560 100 0 100"
-        ></polygon>
-      </svg>
-    </div>
-
-    <div className="container mx-auto px-4">
-      <div className="items-center flex flex-wrap">
-        <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
-          <img
-            alt="..."
-            className="max-w-full rounded-lg shadow-lg"
-            src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-          />
-        </div>
-        <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
-          <div className="md:pr-12">
-            <div className="text-pink-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-pink-300">
-              <i className="fas fa-rocket text-xl"></i>
-            </div>
-            <h3 className="text-3xl font-semibold">A growing company</h3>
-            <p className="mt-4 text-lg leading-relaxed text-gray-600">
-              The extension comes with three pre-built pages to help you get
-              started faster. You can change the text and images and you're good
-              to go.
-            </p>
-            <ul className="list-none mt-6">
-              <li className="py-2">
-                <div className="flex items-center">
-                  <div>
-                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
-                      <i className="fas fa-fingerprint"></i>
-                    </span>
-                  </div>
-                  <div>
-                    <h4 className="text-gray-600">
-                      Carefully crafted components
-                    </h4>
-                  </div>
-                </div>
-              </li>
-              <li className="py-2">
-                <div className="flex items-center">
-                  <div>
-                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
-                      <i className="fab fa-html5"></i>
-                    </span>
-                  </div>
-                  <div>
-                    <h4 className="text-gray-600">Amazing page examples</h4>
-                  </div>
-                </div>
-              </li>
-              <li className="py-2">
-                <div className="flex items-center">
-                  <div>
-                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
-                      <i className="far fa-paper-plane"></i>
-                    </span>
-                  </div>
-                  <div>
-                    <h4 className="text-gray-600">Dynamic components</h4>
-                  </div>
-                </div>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
@@ -279,7 +184,7 @@ const Team = () => (
     <div className="container mx-auto px-4">
       <div className="flex flex-wrap justify-center text-center mb-24">
         <div className="w-full lg:w-6/12 px-4">
-          <h2 className="text-4xl font-semibold">Here are our heroes</h2>
+          <h2 className="text-4xl font-semibold">Here is your mentor</h2>
           <p className="text-lg leading-relaxed m-4 text-gray-600">
             According to the National Oceanic and Atmospheric Administration,
             Ted, Scambos, NSIDClead scentist, puts the potentially record
@@ -288,7 +193,7 @@ const Team = () => (
         </div>
       </div>
       <div className="flex flex-wrap">
-        <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+        <div className="w-full md:w-12/12 lg:w-12/12 lg:mb-0 mb-12 px-4">
           <div className="px-6">
             <img
               alt="..."
@@ -304,24 +209,27 @@ const Team = () => (
                 Web3 Developer
               </p>
               <div className="mt-6">
-                <button
-                  className="bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                <Link
+                  className="bg-blue-400 text-white w-10 h-10 rounded-full outline-none focus:outline-none mr-2 p-3"
                   type="button"
+                  href={"https://twitter.com/fabio_oliveirazz"}
                 >
-                  <i className="fab fa-twitter"></i>
-                </button>
-                <button
-                  className="bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                  <img src="/twitter.svg" />
+                </Link>
+                <Link
+                  className="bg-blue-600 text-white w-10 h-10 rounded-full outline-none focus:outline-none mr-2 p-3"
                   type="button"
+                  href={"https://www.facebook.com/fabio.oliveira.7zz"}
                 >
-                  <i className="fab fa-facebook-f"></i>
-                </button>
-                <button
-                  className="bg-pink-500 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                  <img src="/facebook.svg" />
+                </Link>
+                <Link
+                  className="bg-pink-500 text-white w-10 h-10 rounded-full outline-none focus:outline-none mr-2 p-3"
                   type="button"
+                  href={"https://www.instagram.com/fabio_oliveirazz/"}
                 >
-                  <i className="fab fa-dribbble"></i>
-                </button>
+                  <img src="/instagram.svg" />
+                </Link>
               </div>
             </div>
           </div>
@@ -332,76 +240,103 @@ const Team = () => (
 );
 
 const Finisher = () => (
-  <section className="pb-20 relative block bg-gray-900">
-    <div
-      className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
-      style={{ height: "80px" }}
-    >
-      <svg
-        className="absolute bottom-0 overflow-hidden"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="none"
-        version="1.1"
-        viewBox="0 0 2560 100"
-        x="0"
-        y="0"
+  <>
+    <section className="pb-20 relative block bg-gray-900">
+      <div
+        className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
+        style={{ height: "80px" }}
       >
-        <polygon
-          className="text-gray-900 fill-current"
-          points="2560 0 2560 100 0 100"
-        ></polygon>
-      </svg>
-    </div>
+        <svg
+          className="absolute bottom-0 overflow-hidden"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+          version="1.1"
+          viewBox="0 0 2560 100"
+          x="0"
+          y="0"
+        >
+          <polygon
+            className="text-gray-900 fill-current"
+            points="2560 0 2560 100 0 100"
+          ></polygon>
+        </svg>
+      </div>
 
-    <div className="container mx-auto px-4 lg:pt-24 lg:pb-64">
-      <div className="flex flex-wrap text-center justify-center">
-        <div className="w-full lg:w-6/12 px-4">
-          <h2 className="text-4xl font-semibold text-white">Build something</h2>
-          <p className="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
-            Put the potentially record low maximum sea ice extent tihs year down
-            to low ice. According to the National Oceanic and Atmospheric
-            Administration, Ted, Scambos.
-          </p>
+      <div className="container mx-auto px-4 lg:pt-24 lg:pb-64">
+        <div className="flex flex-wrap text-center justify-center">
+          <div className="w-full lg:w-6/12 px-4">
+            <h2 className="text-4xl font-semibold text-white">
+              Exclusive Collection
+            </h2>
+            <p className="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
+              We'll haveonly 1,000 seats in our rocket, limited to our NFT
+              collection avatars. You will come in?
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-wrap mt-12 justify-center">
+          <div className="w-full lg:w-3/12 px-4 text-center">
+            <div className="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
+              <img src="/gift.svg" />
+            </div>
+            <h6 className="text-xl mt-5 font-semibold text-white">
+              Mint First
+            </h6>
+            <p className="mt-2 mb-4 text-gray-500">
+              The best moment to enter is now. Mint your NFT and join us to grow
+              together.
+            </p>
+          </div>
+          <div className="w-full lg:w-3/12 px-4 text-center">
+            <div className="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
+              <img src="/shopping-cart.svg" />
+            </div>
+            <h5 className="text-xl mt-5 font-semibold text-white">
+              Sell Later
+            </h5>
+            <p className="mt-2 mb-4 text-gray-500">
+              You're ready tp fly alone? Sell your token at OpenSea, probaly by
+              higher prices.
+            </p>
+          </div>
+          <div className="w-full lg:w-3/12 px-4 text-center">
+            <div className="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
+              <img src="/calendar.svg" />
+            </div>
+            <h5 className="text-xl mt-5 font-semibold text-white">
+              Enjoy Forever
+            </h5>
+            <p className="mt-2 mb-4 text-gray-500">
+              Some benefits stay forever for all people that had our tokens.
+            </p>
+          </div>
         </div>
       </div>
-      <div className="flex flex-wrap mt-12 justify-center">
-        <div className="w-full lg:w-3/12 px-4 text-center">
-          <div className="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-            <i className="fas fa-medal text-xl"></i>
+    </section>
+    <section className="pt-20 pb-48">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap justify-center text-center mb-24">
+          <div className="w-full lg:w-6/12 px-4">
+            <h2 className="text-4xl font-semibold">Don't wait More</h2>
+            <p className="text-lg leading-relaxed m-4 text-gray-600">
+              The time is runnning and the collection is limited. Mint one or
+              more tokens right now and join us!
+            </p>
           </div>
-          <h6 className="text-xl mt-5 font-semibold text-white">
-            Excelent Services
-          </h6>
-          <p className="mt-2 mb-4 text-gray-500">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
         </div>
-        <div className="w-full lg:w-3/12 px-4 text-center">
-          <div className="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-            <i className="fas fa-poll text-xl"></i>
+        <div className="flex flex-wrap justify-center text-center mb-24">
+          <div className="w-full lg:w-6/12 px-4">
+            <Link
+              className="bg-black text-white font-bold px-3 py-3 rounded inline-flex items-center ml-3"
+              href={"/mint"}
+            >
+              Mint Now
+            </Link>
           </div>
-          <h5 className="text-xl mt-5 font-semibold text-white">
-            Grow your market
-          </h5>
-          <p className="mt-2 mb-4 text-gray-500">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-        </div>
-        <div className="w-full lg:w-3/12 px-4 text-center">
-          <div className="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-            <i className="fas fa-lightbulb text-xl"></i>
-          </div>
-          <h5 className="text-xl mt-5 font-semibold text-white">Launch time</h5>
-          <p className="mt-2 mb-4 text-gray-500">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </>
 );
 
 // import Head from "next/head";
