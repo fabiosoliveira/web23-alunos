373bdc9a79538ee4ece0344619a7a4995f003489
Fábio <fabio.oliveira@poscontrole.com.br>
2023-12-08 10:56:20 -0300

    Implementação Contrato com OpenZeppelin

--- multitoken-erc1155/contracts/Multitoken.sol
+++ multitoken-erc1155/contracts/Multitoken.sol
@@ -6,25 +6,44 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/String.sol";
 
-contract Multitoken is ERC1155, ERC1155Burnable, Ownable {
-    constructor(address initialOwner)
-        ERC1155("https://www.fabio.com.br/tokens/")
-        Ownable(initialOwner)
-    {}
+contract Multitoken is ERC1155, ERC1155Burnable {
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data)
-        public
-        onlyOwner
-    {
-        _mint(account, id, amount, data);
+    uint public constant NFT_0 = 0;
+    uint public constant NFT_1 = 1;
+    uint public constant NFT_2 = 2;
+
+    uint[] public currentSupply = [50, 50, 50];
+
+    uint public tokenPrice = 0.01 ether;
+
+    string public constant BASE_URL = "https://www.fabio.com.br/tokens/";
+
+    address payable public immutable owner;
+
+    constructor() ERC1155(BASE_URL) {
+        owner = payable(msg.sender);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
-    {
-        _mintBatch(to, ids, amounts, data);
+    function mint(uint256 id) external payable {
+        require(id < 3, "This token does not exist");
+        require(msg.value >= tokenPrice, "Insuficient payment");
+        require(currentSupply[id] > 0, "Max supply reached");
+
+        _mint(msg.sender, id, 1, "");
+        currentSupply[id]--;
+    }
+
+    function uri(uint256 id) public pure override returns (string memory) {
+        return string.concat(BASE_URL, Strings.toString(id), ".json");
+    }
+
+    function withdraw() external {
+        require(msg.sender == owner, "You do not have permission");
+
+        uint256 amount = address(this).balance;
+        (bool success, ) = owner.call{value: amount}("");
+        require(success == true, "Failed to withdraw");
     }
 }
