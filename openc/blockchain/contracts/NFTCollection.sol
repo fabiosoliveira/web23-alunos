330eb88c596ee69fef4054b4a05b8bbe8619a77e
Fábio <fabio.oliveira@poscontrole.com.br>
2024-01-19 15:40:24 -0300

    Smart Contract da Coleção

---
+++ openc/blockchain/contracts/NFTCollection.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFTMarket is ERC721URIStorage {
+    
+    uint private _tokenIds;
+
+    address contractAddress; //marketplace
+    address owner;
+
+    constructor(address marketplaceAddress) ERC721("OpenC", "OPC") {
+        contractAddress = marketplaceAddress;
+        owner = msg.sender;
+    }
+
+    function mint(string memory url) public returns (uint) {
+        uint tokenId = ++_tokenIds;
+
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, url);
+        setApprovalForAll(contractAddress, true);
+
+        return tokenId;
+    }
+
+    function setApprovalForAll(address operator, bool approved) public virtual override {
+        require(_msgSender() == owner || operator != contractAddress || approved, "Cannot remove marketplace approval");
+
+        _setApprovalForAll(_msgSender(), operator, approved);
+    }
+}
