b8e4bf5758af5ea8cf9cd827b3bf1bf5c52e7986
Fábio <fabio.oliveira@poscontrole.com.br>
2024-02-23 15:38:12 -0300

    Subscribe e Unsubscribe de consumers

---
+++ algodollar/blockchain/contracts/IOracleConsumer.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+interface IOracleConsumer {
+
+    event Updated(uint indexed timestamp, uint oldSupply, uint newSupply);
+    
+    function update(uint weisPerPenny) external;
+
+}
--- algodollar/blockchain/contracts/WeiUsdOracle.sol
+++ algodollar/blockchain/contracts/WeiUsdOracle.sol
@@ -1,9 +1,79 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract WeiUsdOracle {
-   
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IOracle.sol";
+import "./IOracleConsumer.sol";
+
+contract WeiUsdOracle is IOracle, Ownable {
+    
+    uint private lastRatio = 0;
+    uint public lastUpdate = 0;
+    address[] public subscribers;
+
+    constructor(uint ethPriceInPenny) Ownable(msg.sender) {
+        uint weisPerPenny = calcWeiRatio(ethPriceInPenny);
+        lastRatio = weisPerPenny;
+        lastUpdate = block.timestamp;
+    }
+
+    function calcWeiRatio(uint ethPriceInPenny) internal pure returns (uint) {
+        return (10 ** 18) / ethPriceInPenny;
+    }
+
+    function setEthPrice(uint ethPriceInPenny) external{
+        require(ethPriceInPenny > 0, "ETH price cannot be zero");
+
+        uint weisPerPenny = calcWeiRatio(ethPriceInPenny);
+        require(weisPerPenny > 0, "Wei Ratio cannot be zero");
+
+        lastRatio = weisPerPenny;
+        lastUpdate = block.timestamp;
+
+        for(uint i=0; i < subscribers.length; ++i){
+            if(subscribers[i] != address(0)){
+                IOracleConsumer(subscribers[i]).update(weisPerPenny);
+            }
+        }
+
+        if(subscribers.length > 0)
+            emit AllUpdated(subscribers);
+    }
+
+    function getWeiRatio() external view returns (uint){
+        return lastRatio;
+    }
+
+    function subscribe(address subscriber) external onlyOwner {
+        require(subscriber != address(0), "Subscriber cannot be zero");
+        emit Subscribed(subscriber);
+
+        for(uint i=0; i < subscribers.length; ++i){
+            if(subscribers[i] == address(0)){
+                subscribers[i] = subscriber;
+                return;
+            }
+            else if(subscribers[i] == subscriber){
+                return;
+            }
+        }
+
+        subscribers.push(subscriber);
+    }
+
+    function unsubscribe(address subscriber) external onlyOwner {
+        require(subscriber != address(0), "Subscriber cannot be zero");
+
+        for(uint i=0; i < subscribers.length; ++i){
+            if(subscribers[i] == subscriber){
+                delete subscribers[i];
+                emit Unsubscribed(subscriber);
+                return;
+            }
+        }
+    }
+
 }
